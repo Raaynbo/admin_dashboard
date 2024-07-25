78771dc720e0479f335dae571e81a6f6e21e1ada
Raaynbo <rayan.boudjema24@gmail.com>
2024-07-25 15:33:25 +0200

    community project tile modified to present only 2 project

---
+++ main.js
@@ -0,0 +1,9 @@
+const featuredProject = []
+const ftPjt1 = document.querySelector(".disabled");
+
+featuredProject.push(ftPjt1);
+
+const side_cards = document.querySelectorAll(".side_card_content");
+
+side_cards.forEach(card => {
+});
